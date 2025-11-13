6d9cd4649280b164d4bfa84134a5e92be14820df
Md.Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-13 16:25:52 +0600

    new add 13

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./*.html", "./pages/**/*.html"],
+  content: ["./*.html", "./pages/**/*.html",
+            './frontend/components/**/*.html',
+  ],
   theme: {
     extend: {
       colors: {
